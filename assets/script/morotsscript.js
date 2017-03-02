6fc8f21dc1bf998e0a7b607bea7e79cd3c58dafa
Wedsa95 <jonasolsson1995@gmail.com>
2017-03-02 15:43:54 +0100

    final Webdev assignment done

--- assets/script/morotsscript.js
+++ assets/script/morotsscript.js
@@ -1,18 +1,11 @@
-window.onloads = loadTotalRating();
 var json;
-
 var recipe__numbers = document.getElementsByClassName("recipe__numbers");
-var recipe__one__batch =  [100,2,2,1,0.5,1,1,2,100,50,2,16,12];
+var recipe__one__batch =  [100, 2, 2, 1, 0.5, 1, 1, 2, 100, 50, 2, 16, 12];
 var recipe__input = document.getElementById("recipe__input");
 var all__voters = document.getElementById("all__voters");
 var your__vote = document.getElementById("your__vote");
-var stars = document.getElementsByClassName("stars");
+var stars = document.getElementsByClassName("material-icons gray");
 
-document.getElementById("recipe__input")
-.addEventListener('change', inputBoxChange);
-for(var i = 0; i < stars.length; ++i){
-	stars[i].addEventListener('click', sendVoteScore);
-}
 
 
 function inputBoxChange(){
@@ -37,18 +30,43 @@ function inputBoxChange(){
 			}
 	}
 }
-function sendVoteScore(e){
-	if(stars[0] == e){
-		console.log("star1");
-	}else if(stars[1]){
-		
-	}else if(stars[2]){
-		
-	}else if(stars[3]){
-		
-	}else if(stars[4]){
-		
+function starChange(ratingValue){
+		for(var i = 0; i < ratingValue; i++){
+			stars[i].style.animationName = "icons";
+			stars[i].style.color = "green";
+		}
 	}
+function sendVoteScore(e){
+	var ratingValue;
+	
+	if(this == stars[0]){
+		ratingValue = "1";
+	}else if(this == stars[1]){
+		ratingValue = "2";
+	}else if(this == stars[2]){
+		ratingValue = "3";
+	}else if(this == stars[3]){
+		ratingValue = "4";
+	}else if(this == stars[4]){
+		ratingValue = "5";
+	}else{ratingValue = "5";}
+	
+	starChange(ratingValue);
+	
+	console.log(ratingValue);
+	var sendRating = new XMLHttpRequest();
+	
+	sendRating.onreadystatechange = function(){
+		
+		if(this.readyState === 4 && this.status === 200){
+			loadTotalRating();
+				console.log("ggggg");
+		}
+	}
+	sendRating.open("GET",
+	"https://edu.oscarb.se/sjk15/api/recipe/?api_key=9e721dd3ec43ce1d&recipe=morrotskaka&rating=" + ratingValue
+	, true);
+	sendRating.send();
 	
 }
 function loadTotalRating(){
@@ -64,7 +82,7 @@ function loadTotalRating(){
 			json = JSON.parse(response);
 			
 			all__voters.innerHTML = json.votes;
-			your__vote.innerHTML = json.rating;
+			your__vote.innerHTML = json.rating.toFixed(1);
 			console.log(json);
 		}
 	}
@@ -75,3 +93,14 @@ function loadTotalRating(){
 	xhttp.send();
 	
 }
+window.onloads = function(){
+	document.getElementById("recipe__input")
+	.addEventListener("change", inputBoxChange, false);
+
+	for(var i = 0; i < stars.length; ++i){
+		stars[i].addEventListener("click", sendVoteScore, false);
+		starArray[i] = stars[i] + i;
+	}
+
+}
+window.onloads = loadTotalRating();
